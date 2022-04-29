93f745368f53a190d53df40af622e27b28c4aa63
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-29 20:03:42 -0300

    requisito 2, 9 e 6

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const botaoEsvazia = document.querySelector('.empty-cart');
+const carrinho = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,6 +15,31 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // pipipi popopo
+ }
+ 
+ function createCartItemElement({ sku, name, salePrice }) {
+   const li = document.createElement('li');
+   li.className = 'cart__item';
+   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+   li.addEventListener('click', cartItemClickListener);
+   return li;
+ }
+
+ async function addAoCarrinho(event) {
+  const produto = event.target.parentElement.querySelector('.item__sku').innerText;
+  const resultadosItem = await fetchItem(produto);
+  console.log(resultadosItem);
+  const cadaItem = { 
+  sku: resultadosItem.id,
+  name: resultadosItem.title,
+  salePrice: resultadosItem.price,
+};
+const criaItem = createCartItemElement(cadaItem);
+carrinho.appendChild(criaItem);
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,38 +49,14 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  const add = document.querySelectorAll('.item__add');
+  add.forEach((botao) => botao.addEventListener('click', addAoCarrinho));
+
   return section;
 }
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-async function cartItemClickListener(event) {
- const produto = event.target.parentElement.querySelector('.item__sku').innerText;
- const lista = document.querySelector('.cart__items');
- const resultadosItem = await fetchItem(produto);
- const cadaItem = { 
-  sku: resultadosItem.id,
-  name: resultadosItem.title,
-  salePrice: resultadosItem.price,
-};
-// eslint-disable-next-line no-use-before-define
-const criaItem = createCartItemElement(cadaItem);
-  lista.appendChild(criaItem);
-}
-
-function addEventos() {
-  const add = document.querySelectorAll('.item__add');
-  add.forEach((botao) => botao.addEventListener('click', cartItemClickListener));
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 const chamaFetchProduct = async () => {
@@ -67,6 +71,13 @@ const chamaFetchProduct = async () => {
   });
 };
 
+    botaoEsvazia.addEventListener('click', () => {
+    carrinho.innerText = '';
+    localStorage.clear();
+  });
+
+// const cart = document.querySelectorA('.cart__items');
+
 /* A função chamaFetchProduct vai buscar a section que contém os elementos com a classe items,
 vai chamar o resultado da FetchProducts já com o parâmetro 'computador', da categoria que queremos
 e armazenar em uma constante. Então ela vai acessar o array results do objeto json e para cada resultado,
@@ -76,7 +87,6 @@ acessado no array results, onde precisamos do id, do title e da thumbnail. Entã
 criarElementos no elemento pai sessaoElementos, colocando os produtos na tela do site.
 */
 
-window.onload = async () => {
-  await chamaFetchProduct();
-  addEventos();
+window.onload = () => {
+  chamaFetchProduct();
 };
