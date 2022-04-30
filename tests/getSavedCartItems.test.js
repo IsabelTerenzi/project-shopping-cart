fd3efd5772bfc1e890c1b9b8c904e71e296020e5
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-30 13:13:18 -0300

    requisito 11 sem require

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,6 +1,6 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
-const { get } = require('jquery');
+// const { get } = require('jquery');
 
 localStorageSimulator('getItem');
 
