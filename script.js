53a72b57ad772632bb1417e021b2186b72b18447
Isabel Terenzi <bel.terenzi@gmail.com>
2022-05-02 18:24:30 -0300

    requisito 7

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
- 
+ /* funçao que cria um evento de clique para que toda vez que um item do carrinho for clicado,
+ ele ser removido do carrinho. E também chama a saveCartItems, para que ao clicar no item para
+ removê-lo, também o remove do local storage.
+ */
 function cartItemClickListener(event) {
   event.target.remove();
   saveCartItems(cartItems.innerHTML);
@@ -28,6 +31,15 @@ function cartItemClickListener(event) {
    return li;
  }
 
+ /* a função é chamada na createProductItemElement, recebendo um evento como parâmetro, que
+ ao acionado, busca o elemento pai de onde foi clicado, ou seja, o id (sku), o title (name)
+ e a thumbnail (image) de cada produto, buscando o innerText disso, aguardando a fetchItem,
+ que vai fazer a requisição da API pelo id, e então para irá armazenar em uma constante apenas
+ o id, o nome e o preço de cada produto, para adicionar ao carrinho. Então esse elemento será
+ appendado para que apareçam no carrinho. Com a criação desses elementos, a função que salva
+ no local storage também é chamada, para que os itens do carrinho sejam salvos ĺá.
+ */
+
  async function addAoCarrinho(event) {
   const produto = event.target.parentElement.querySelector('.item__sku').innerText;
   const resultadosItem = await fetchItem(produto);
@@ -40,6 +52,11 @@ const criaItem = createCartItemElement(cadaItem);
 cartItems.appendChild(criaItem);
 saveCartItems(cartItems.innerHTML);
 }
+
+/* função já implementada com o projeto, com a adição de uma constante add que busca o botão de adicionar
+ao carrinho e cria o evento de clique para cada um deles, a medida que os elementos da lista de produtos
+são criados.
+*/
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -60,23 +77,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-const chamaFetchProduct = async () => {
-  const sessaoElementos = document.querySelector('.items');
-  const resultados = await fetchProducts('computador');
-  resultados.results.forEach((resultado) => {
-    const criarElementos = createProductItemElement({ sku: resultado.id,
-      name: resultado.title,
-      image: resultado.thumbnail,
-});
-    sessaoElementos.appendChild(criarElementos); 
-  });
-};
-
-  botaoEsvazia.addEventListener('click', () => {
-    cartItems.innerHTML = '';
-    localStorage.clear();
-  });
-
 /* A função chamaFetchProduct vai buscar a section que contém os elementos com a classe items,
 vai chamar o resultado da FetchProducts já com o parâmetro 'computador', da categoria que queremos
 e armazenar em uma constante. Então ela vai acessar o array results do objeto json e para cada resultado,
@@ -86,8 +86,38 @@ acessado no array results, onde precisamos do id, do title e da thumbnail. Entã
 criarElementos no elemento pai sessaoElementos, colocando os produtos na tela do site.
 */
 
+async function chamaFetchProducts() {
+  const sessaoElementos = document.querySelector('.items');
+  const resultados = await fetchProducts('computador');
+  resultados.results.forEach((resultado) => {
+    const criarElementos = createProductItemElement({ sku: resultado.id,
+      name: resultado.title,
+      image: resultado.thumbnail,
+});
+    sessaoElementos.appendChild(criarElementos);
+  });
+}
+
+function enquantoCarrega() {
+  const sessaoElementos = document.querySelector('.items');
+  const texto = document.createElement('p');
+  texto.classList = 'loading';
+  texto.innerText = 'carregando...';
+  sessaoElementos.appendChild(texto);
+  chamaFetchProducts();
+  texto.remove();
+}
+
+/* ação implementada no botão 'esvaziar carrinho', chamado no topo do arquivo, com a constante botaoEsvazia,
+adicionando um clique que limpa o HTML inteiro do carrinho, além de limpar o local storage.
+*/
+  botaoEsvazia.addEventListener('click', () => {
+    cartItems.innerHTML = '';
+    localStorage.clear();
+  });
+
 window.onload = () => {
-  chamaFetchProduct();
+  enquantoCarrega();
   cartItems.innerHTML = getSavedCartItems();
   const itens = document.querySelectorAll('.cart__item');
   itens.forEach((botao) => botao.addEventListener('click', cartItemClickListener));
